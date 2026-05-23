db1a4ca5aec776ebe3f5ee88ef2023b82e20c889
gamer5000m-dev <gamer5000m@gmail.com>
2026-05-23 13:06:53 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,34 @@
+import threading
 import time
+from flask import Flask
 
-print("INICIANDO SISTEMA...")
-print("BOT INICIADO")
+# ================= FLASK =================
 
-while True:
-    print("BOT RODANDO NORMALMENTE")
-    time.sleep(60)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot rodando OK"
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+# ================= BOT =================
+
+def bot():
+    print("BOT INICIADO")
+
+    while True:
+        print("BOT RODANDO...")
+        time.sleep(60)
+
+# ================= START =================
+
+if __name__ == "__main__":
+    print("INICIANDO SISTEMA...")
+
+    threading.Thread(target=run_web, daemon=True).start()
+    threading.Thread(target=bot, daemon=True).start()
+
+    while True:
+        time.sleep(999999)
