293e5b3bffe7bd945bd2842c6e2742d335523d27
gamer5000m-dev <gamer5000m@gmail.com>
2026-05-23 12:21:53 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ from telegram.request import HTTPXRequest
 # CONFIG
 # =========================================================
 
-TOKEN = "SEU_TOKEN_AQUI"
-CHAT_ID = -1001234567890
+TOKEN = "8391542912:AAH1cduJ0E7naPhA0z6uezCgkbLn1BjyQDE"
+CHAT_ID = -1003914285353
 
 URL = "https://www.promobit.com.br"
 TEMPO = 300
